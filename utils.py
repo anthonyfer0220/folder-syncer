e8a5029758a3c50c5046ef96e93546da3fcc04d0
Anthony Fernandez <anthonyfer0220@gmail.com>
2025-01-07 15:25:06 -0500

    Refactor deletion logic in sync function
    Moved file and directory deletion logic into a separate 'delete'
    function. The function dynamically determines the item type, handles
    user confirmation before deletion, and it logs deleted items and errors

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def sync_folders(src, dest, skipping):
     Synchronize source folder with destination folder
 
     Copies new or updated files and directories from source to destination
-    Remove files and directories in the destination folder that do not exist in the source
+    Delete files and directories in the destination folder that do not exist in the source
 
     Extra:
     Skip specific files and/or directories from the source folder
@@ -70,16 +70,54 @@ def sync_folders(src, dest, skipping):
                 logging.info(f"Copied file: {src_path} -> {dest_path}")
 
     # Iterate through items in destination folder
-    # and delete those that do not exist in source folder
+    # and delete those not in source folder
     for item in dest_items:
 
         src_path = Path(src) / item
         dest_path = Path(dest) / item
 
         if item in skipping or not src_path.exists():
-            if dest_path.is_dir():
-                shutil.rmtree(dest_path)
-                logging.info(f"Removed directory: {dest_path}")
-            else:
-                dest_path.unlink()
-                logging.info(f"Removed file: {dest_path}")
+            delete(dest_path) # Delete file or directory
+
+
+
+def delete(dest):
+    """
+    Delete files or directories with user confirmation
+
+    Informs user of file or directory to be deleted
+    Validate user's input
+    Performs specific deletion depending on the destination path being a file or a directory
+
+    Args:
+        dest (Path): The file or directory to be deleted
+
+    Returns:
+        None
+    """
+
+    if dest.is_dir():
+        item_type = "directory"
+    else:
+        item_type = "file"
+
+    valid_response = False
+
+    # Validate user's input
+    while not valid_response:
+        usr_input = input(f"The following {item_type} will be deleted: {dest}\n"
+                          f"Do you wish to proceed? (y/n) ")
+        if usr_input.lower() == "y": # Perform deletion
+            try:
+                if item_type == "directory":
+                    shutil.rmtree(dest)
+                elif item_type == "file":
+                    dest.unlink()
+                logging.info(f"Deleted {item_type}: {dest}")
+            except Exception as e:
+                logging.error(f"Failed to delete {item_type}: {dest}. Error: {e}")
+            valid_response = True
+        elif usr_input.lower() == "n":  # Skip deletion
+            break
+        else:
+            print("Please enter a valid response.")
