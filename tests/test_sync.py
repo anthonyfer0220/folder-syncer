b5fa71380cef18fe92a2f269f7818c833ff23bb7
Anthony Fernandez <anthonyfer0220@gmail.com>
2025-01-13 15:47:43 -0500

    Add TODOs for sync and sysexit test cases

---
+++ tests/test_sync.py
@@ -0,0 +1,4 @@
+# TODO: Test copying new files
+# TODO: Test deleting files in target
+# TODO: Test updating files
+# TODO: Test exclusion of files
---
+++ tests/test_sysexit.py
@@ -0,0 +1,4 @@
+# TODO: Test number or arguments
+# TODO: Test when source directory doesn't exist
+# TODO: Test when destination exists but it's not a directory
+# TODO: Test when source and destination directory are the same
